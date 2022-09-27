15232d15e910d600a2484304027472fc5d599d0a
igorkurnikov <60519686+igorkurnikov@users.noreply.github.com>
2022-09-27 13:28:45 -0400

    Fixes to download files using selenium web driver

--- biblpy/__init__.py
+++ biblpy/__init__.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import requests
+import time
+import shutil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 from .webofscience import * 
 from .gscholar import *
 from .scidir import *
@@ -33,15 +37,68 @@ def InitWebDriver():
     driver = webdriver.Chrome(options=options)
     return driver
 
+def get_webdriver():
+    if( not driver or not is_webdriver_alive(driver) ): biblpy.driver = InitWebDriver()
+    return biblpy.driver
+
 def StopWebDriver():
     if( driver ): driver.quit()
     driver = None
 
-def save_url_to_file( url, fname ):
+def is_webdriver_alive(driver):
+
+    print('Checking whether the driver is alive')
+    try:
+        assert(driver.service.process.poll() == None) #Returns an int if dead and None if alive
+        driver.service.assert_process_still_running() #Throws a WebDriverException if dead
+        driver.find_element(By.TAG_NAME,'html') #Throws a NoSuchElementException if dead
+        print('The driver appears to be alive')
+        return True
+    except (NoSuchElementException, WebDriverException, AssertionError):
+        print('The driver appears to be dead')
+        return False
+    except Exception as ex:
+        print('Encountered an unexpected exception type ({}) while checking the driver status'.format(type(ex)))
+        return False
+
+def getDownLoadedFileName(driver, waitTime):
+    #driver.execute_script("window.open()")
+    # switch to new tab
+    #driver.switch_to.window(driver.window_handles[-1])
+    # navigate to chrome downloads
+    driver.get('chrome://downloads')
+    # define the endTime
+    endTime = time.time()+waitTime
+    while True:
+        try:
+            # get downloaded percentage
+            downloadPercentage = driver.execute_script(
+                "return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value")
+            # check if downloadPercentage is 100 (otherwise the script will keep waiting)
+            if downloadPercentage == 100:
+                # return the file name once the download is completed
+                return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+        except:
+            return driver.execute_script("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content  #file-link').text")
+        time.sleep(1)
+        if time.time() > endTime:
+            break
+
+def save_url_to_file_requests( url, fname ):
+        """ Copy a file downloaded from url to fname using requests library """
         r = requests.get(url, allow_redirects=True)
         fout = open(fname,"wb")
         fout.write(r.content)
         fout.close()
+
+def save_url_to_file( url, fname ):
+    """ Copy a file downloaded from url to fname using selenium driver """
+    if( not biblpy.driver or not is_webdriver_alive(biblpy.driver) ): biblpy.driver = InitWebDriver()
+    biblpy.driver.get(url)
+    fn_d = getDownLoadedFileName(biblpy.driver,15)
+    d_dir = get_download_path()
+    fn_d_full = os.path.join(d_dir,fn_d)
+    shutil.copyfile(fn_d_full, fname)
 
 def get_download_path():
     """Returns the default downloads path for linux or windows"""
