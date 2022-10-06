7db3078803f35d472f86c70c025db8e97db1d85e
igorkurnikov <60519686+igorkurnikov@users.noreply.github.com>
2022-10-05 20:43:08 -0400

    fix to the Google Scholar Dialog

--- biblpy/gs/gs_ref_dlg.py
+++ biblpy/gs/gs_ref_dlg.py
@@ -1,13 +1,15 @@
 from .gs_ref_dlg_GUI import *
 
 class GSRefDlg(gs_ref_dlg_GUI):
+    
     def __init__(self,parent=None,id=-1,title="Google Scholar Dialog"):
-        wx.Frame.__init__(self, parent, id, title, wx.DefaultPosition, wx.Size(300, 150))
+    #    wx.Frame.__init__(self, parent, id, title, wx.DefaultPosition, wx.Size(300, 150))
+        parent = wx.FindWindowByName("Bibliographic Database")
+        gs_ref_dlg_GUI.__init__(self,parent,id,title,wx.DefaultPosition, wx.Size(300, 350))
 
-        panel = wx.Panel(self,-1)
-
-        self.btn1 = wx.Button(self,id = wx.ID_ANY, label = "test1")
-        self.btn1.Bind(wx.EVT_BUTTON, self.OnTest1)
+    #    panel = wx.Panel(self,-1)
+    #    self.btn1 = wx.Button(self,id = wx.ID_ANY, label = "test1")
+    #    self.btn1.Bind(wx.EVT_BUTTON, self.OnTest1)
     
     def OnTest1(self,event):
         print("test1")
